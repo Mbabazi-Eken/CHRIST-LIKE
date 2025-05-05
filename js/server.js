7e051cea8be06b9a7a3cfc41cf0113a4e742dd31
MbabaziEken <mbabazieken@gmail.com>
2025-05-05 15:27:01 +0300

    x

--- js/server.js
+++ js/server.js
@@ -1,18 +1,13 @@
-// server.js
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-app.use(express.static(__dirname + '/public'));
-io.on('connection', function(socket) {
-  console.log('a new client connected');
-  socket.on('new comment', function(comment) {
-    io.emit('new comment', comment);
-  });
-  socket.on('disconnect', function() {
-    console.log('a client disconnected');
-  });
-});
-server.listen(3000, function() {
-  console.log('server listening on port 3000');
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port:8080 });
+wss.on('connection', (ws) => {
+ console.log('a new client connected');
+ ws.on('message', (message) => {
+ console.log(`received message`  + message);
+ wss.clients.forEach((client) => {
+ if (client !== ws && client.readyState === WebSocket.OPEN) {
+ client.send(message);
+ }
+ });
+ });
 });
